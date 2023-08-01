3070a78310af94134310b72224b5872c679f701b
user <rusobraz@mail.ru>
2023-08-01 23:19:35 +0300

    Решение задачи билета №5

--- src/pro/sky/java/courseworkticket/Solution.java
+++ src/pro/sky/java/courseworkticket/Solution.java
@@ -1,4 +1,28 @@
 package pro.sky.java.courseworkticket;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getArray(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})));
+    }
+
+    public static int[] getArray(int[] arr) {
+        int first = 3;
+        int last = 7;
+        if (arr.length < last) {
+            throw new IllegalArgumentException("Не верный размер массива.");
+        }
+        int[] result = new int[5];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i]++;
+            }
+            if (i >= first - 1 && i < last) {
+                result[index++] = arr[i];
+            }
+        }
+        return result;
+    }
 }
